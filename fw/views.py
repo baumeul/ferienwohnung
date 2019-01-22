d8797dd7c6866b8c04405a619b2d9e62b466554d
baumeul <ulrich.baumeister@me.com>
2019-01-22 19:07:43 +0100

    Erstes Template

--- fw/views.py
+++ fw/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Gast
+
 
 def fw_list(request):
-    return render(request, 'fw/fw_list.html', {})
+    gaeste = Gast.objects.filter(created__lte=timezone.now()).order_by('created')
+    return render(request, 'fw/fw_list.html', {'gaeste': gaeste})
